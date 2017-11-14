ed003ad6b5844d5edd7fda15072773500d014fe9
Roger Day <notsofeo@gmail.com>
2017-11-14 00:19:14 -0500

    modifications to ecmo bayes, don't know if it's quite ready.

--- ecmo bayes.R
+++ ecmo bayes.R
@@ -1,10 +1,12 @@
 ecmo2Data = matrix(c(9, 0, 6, 4), nrow=2)
+ecmoProportions = ecmo2Data[1, ]/colSums(ecmo2Data)
+
 ab = c(1,2)
 
 lik2 = function(ab) {
 	a = ab[1]
 	b = ab[2]
-	p = antilogit(a + b*(1:0))
+	p = antilogit(c(a + b, a))
 	prod(
 	  p^ecmo2Data[1,] * (1-p)^ecmo2Data[2,]
 	  )
@@ -16,24 +18,27 @@ fisher.test(ecmo2Data)
 
 require("mvtnorm")
 
-prior2 = function(ab, a0=0, b0=0, va=1, vab=0, vb=1)
+prior2 = function(ab, a0=0, b0=0, va=100, vab=0, vb=100)
 		dmvnorm(ab, c(a0, b0), 
 			matrix(c(va, vab, vab, vb), nrow=2))
 
-avec = seq(-1, 3, length.out=20)
-bvec = seq(-1, 10, length.out=20)
+avec = seq(0, 20, length.out=100)
+bvec = seq(0, 20, length.out=100)
 abgrid = expand.grid(avec, bvec)
-
+pEvec = antilogit(avec)
+pCvec = antilogit(bvec)
+jacobean = outer(pCvec*(1-pCvec) + pEvec*(1-pEvec), pCvec*(1-pCvec))
+  
 par(mfrow=c(1,2))
 
 prior2Matrix = matrix(prior2(abgrid), nrow=length(avec))
 contour(avec, bvec, prior2Matrix)
-
+contour(pEvec, pCvec, prior2Matrix * jacobean)
 lik2Matrix = matrix(apply(abgrid, 1, lik2), 
 	nrow=length(avec))
-contour(avec, bvec, lik2Matrix,
+contour(pEvec, pCvec, lik2Matrix * jacobean,
 	add=T)
-
+points(ecmoProportions, col='red', pch='.', cex=10)
 posterior2Matrix = prior2Matrix * lik2Matrix
 contour(avec, bvec, posterior2Matrix)
 
